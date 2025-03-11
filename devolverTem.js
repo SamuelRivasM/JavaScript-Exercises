614e1344660d85f5d1d8ee9b879ac22f69907bdd
SamuelRivasM <samuelrivasm07@gmail.com>
2025-03-11 00:30:44 -0500

    Función para transformar de Celsius a Fahrenheit

---
+++ devolverTem.js
@@ -0,0 +1,12 @@
+// Crea una función que convierta grados Celsius a Fahrenheit.
+
+let Temp = 20.2;
+
+function CtoF(Temp) {
+    let Fah = (9/5)*Temp+32
+    return Fah
+}
+
+let resultado = CtoF(Temp)
+
+console.log(resultado)
